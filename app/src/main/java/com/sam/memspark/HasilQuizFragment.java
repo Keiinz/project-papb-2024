ff6b610bd7be81e830fed10bb84cf1a48d56e2d8
Keith Evangel Samuel <keithes481@student.ub.ac.id>
2024-11-13 15:48:38 +0700

    Sesudah Konversi Fragment

---
+++ app/src/main/java/com/sam/memspark/HasilQuizFragment.java
@@ -0,0 +1,19 @@
+package com.sam.memspark;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class HasilQuizFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_hasil_quiz, container, false);
+    }
+}
